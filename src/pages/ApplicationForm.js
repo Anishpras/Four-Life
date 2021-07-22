2b1c8f1b1282af032e7c64c42512d09386012e37
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2021-07-22 14:08:57 +0530

    Started working on form

--- src/pages/ApplicationForm.js
+++ src/pages/ApplicationForm.js
@@ -1,7 +1,31 @@
 import React from "react";
 
 const ApplicationForm = () => {
-  return <>hi</>;
+  return (
+    <>
+      <label htmlFor="distributor_id">DistributorId</label>
+      <input type="text" />
+      <label htmlFor="4life_distributor_ID_check">
+        Have you ever held a 4life distributor ID?{" "}
+      </label>
+      <label htmlFor="yes_distributor_id">Yes</label>
+      <input type="checkbox" name="" id="" />
+      <label htmlFor="no_distributor_id">No</label>
+      <input type="checkbox" name="" id="" />
+      <label htmlFor="old_id">If Yes, Old Id</label>
+      <input type="text" />
+
+      <label htmlFor="4life_fssai_reg_check">
+        Do you have a FSSAI registration no ?{" "}
+      </label>
+      <label htmlFor="yes_fssai_reg">Yes</label>
+      <input type="checkbox" name="" id="" />
+      <label htmlFor="no_fssai_reg">No</label>
+      <input type="checkbox" name="" id="" />
+      <label htmlFor="old_id">If Yes, FSSAI registration No.</label>
+      <input type="text" />
+    </>
+  );
 };
 
 export default ApplicationForm;
