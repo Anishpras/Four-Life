86e8734ecc3e1161f548b1f09b550111a1cab973
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2021-08-01 15:59:31 +0530

    bug fixed

--- src/pages/ApplicationForm.js
+++ src/pages/ApplicationForm.js
@@ -137,7 +137,7 @@ const ApplicationForm = () => {
   const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
   function generateString(length) {
-    let result = " ";
+    let result = "";
     const charactersLength = characters.length;
     for (let i = 0; i < length; i++) {
       result += characters.charAt(Math.floor(Math.random() * charactersLength));
